bfbf7cca94efceb4cbbf38acfee85a67c6b920ad
Neeraj Kumar <122194555+yadavneeraj16@users.noreply.github.com>
2024-09-08 19:24:21 +0530

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,7 @@ import { catchAsyncErrors } from "./catchAsyncError.js";
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
-export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+export const isAuthorized = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
